3538584d4af0a30b43505e55fae44ad96dff709e
aguerax9 <aguerax9@gmail.com>
2021-05-19 17:37:49 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  rubiks-solver
+//
+//  Created by William Pale on 19/05/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
